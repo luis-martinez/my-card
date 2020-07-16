b9261b77690ee3008a80851020024767b29b5f4a
Luis Martinez <luis-martinez@users.noreply.github.com>
2020-07-15 17:27:17 -0700

    Added avatar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.blueGrey,
+                backgroundImage: AssetImage('images/avatar.png'),
+              ),
+              Text(
+                'John Doe',
+                style: TextStyle(
+                  fontFamily: 'Pacifico-Regular',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Flutter Developer'.toUpperCase(),
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'SourceSansPro-Regular',
+                    fontSize: 15.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
